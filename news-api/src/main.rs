9f6d0953b52639aff6ba6b1a5da9551ffd39da68
olegarino <otchernov@gmail.com>
2025-10-13 00:39:07 -0400

    Update on basic api and test

--- news-api/src/main.rs
+++ news-api/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use news_api::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    run(TcpListener::bind("127.0.0.1:8000").expect("Failed"))?.await
 }
